19ecca26d5a9ee949b02b1e0b779f49c7222cb08
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2013-07-08 14:18:11 +0400

    just copy-paste XmlParser from std for good

---
+++ src/test/haxe/com/tenderowls/xml176/Xml176ParserTest.hx
@@ -0,0 +1,22 @@
+package com.tenderowls.xml176;
+
+import com.tenderowls.xml176.Xml176Parser.Xml176Document;
+
+class Xml176ParserTest extends haxe.unit.TestCase {
+
+    public function testNode() {
+        var doc:Xml176Document = Xml176Parser.parse("<Node></Node>");
+        assertEquals(
+            Std.string(doc.getNodePosition(doc.document.firstChild())),
+            Std.string({from:1, to:5})
+        );
+    }
+
+    public function testAttr() {
+        var doc:Xml176Document = Xml176Parser.parse("<Node attr=\"value\"></Node>");
+        assertEquals(
+            Std.string(doc.getAttrPosition(doc.document.firstChild(), "attr")), "dsdsad"
+            //Std.string({from:7, to:11})
+        );
+    }
+}
